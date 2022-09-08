e5b9cbacb861cb1105490fc802e39463ffe607fa
Ali-Sufyan <alisufyanofficial0037@gmail.com>
2022-09-08 09:36:53 +0500

    work again

--- src/App.js
+++ src/App.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div>Ali sufyan Gondal is software house work</div>
+    <div>Ali sufyan Gondal is software house work work again and again</div>
   )
 }
